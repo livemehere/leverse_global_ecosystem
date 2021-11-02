7ac87b3512102342d5a51b93a68af5e8f1c997d0
kongtaemin <rhdxoals@gmail.com>
2021-11-02 23:06:09 +0900

    welcome page 완전끝

--- js/index.js
+++ js/index.js
@@ -2,14 +2,9 @@ $(".begin-btn").click(function () {
   $(".wrap").css("transform", "translateX(-100vw)");
 });
 
-var description = document.querySelector('.wrap_description');
-
-var manager = new Hammer.manager(description);
-
-var Swipe = new Hammer.Swipe();
-
-manager.add(Swipe);
-
-manager.on('swipe', function(e) {
-  // umm...
+//  swiper
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });

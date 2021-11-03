1c87eb487bfa52f3e68c4fe6897de79ba9a06bed
kongtaemin <rhdxoals@gmail.com>
2021-11-03 12:50:05 +0900

    main page 카드 슬라이드 완료

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
+
 $(".begin-btn").click(function () {
   $(".wrap").css("transform", "translateX(-100vw)");
 });
---
+++ js/main.js
@@ -0,0 +1,9 @@
+//  swiper
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
